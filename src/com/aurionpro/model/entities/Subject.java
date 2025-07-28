865be76dadacadec74f74cf1ea470eb3e4ad939e
unknown <mohammad.sajid>
2025-07-28 10:42:03 +0530

    model done

---
+++ src/com/aurionpro/model/entities/Subject.java
@@ -0,0 +1,55 @@
+package com.aurionpro.model.entities;
+
+public class Subject {
+	  private int subjectId;
+	    private String name;
+	    private int courseId;   
+	    private int teacherId;  
+
+	    public Subject() {}
+
+	    public Subject(int subjectId, String name, int courseId, int teacherId) {
+	        this.subjectId = subjectId;
+	        this.name = name;
+	        this.courseId = courseId;
+	        this.teacherId = teacherId;
+	    }
+
+	    public int getSubjectId() {
+	        return subjectId;
+	    }
+
+	    public void setSubjectId(int subjectId) {
+	        this.subjectId = subjectId;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getCourseId() {
+	        return courseId;
+	    }
+
+	    public void setCourseId(int courseId) {
+	        this.courseId = courseId;
+	    }
+
+	    public int getTeacherId() {
+	        return teacherId;
+	    }
+
+	    public void setTeacherId(int teacherId) {
+	        this.teacherId = teacherId;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Subject [subjectId=" + subjectId + ", name=" + name +
+	               ", courseId=" + courseId + ", teacherId=" + teacherId + "]";
+	    }
+}
